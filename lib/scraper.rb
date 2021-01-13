44d5e78b51826e4953f8100062daf43922ad7a51
Alex Cline <rev.a.r.cline@gmail.com>
2021-01-13 11:10:03 -0600

    commit tree 7b4417223e7f96f955483577f117a7f4226f3b3d

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,21 +3,21 @@ class Scraper
 
   def initialize(db)
     self.db = db
-    self.file = File.open("pokemon_index.html")
+    self.file = File.open('pokemon_index.html')
     self.parsed_file = Nokogiri::HTML.parse(file)
     self.all_pokemon = catch_em_all
   end
 
   def catch_em_all
-    self.all_pokemon = parsed_file.css(".infocard-tall")
+    self.all_pokemon = parsed_file.css('.infocard-tall')
   end
 
   def get_pokemon_name_from(node)
-    node.css(".ent-name").text
+    node.css('.ent-name').text
   end
 
   def get_pokemon_type_from(node)
-    node.css(".itype").text
+    node.css('.itype').text
   end
 
   def scrape
@@ -27,5 +27,4 @@ class Scraper
       Pokemon.save(pk_name, pk_type, db)
     end
   end
-
 end
