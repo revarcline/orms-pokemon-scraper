af081579d2f77c7f8ace4e08cca440cf6e76421d
Alex Cline <rev.a.r.cline@gmail.com>
2021-01-13 11:10:14 -0600

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,21 @@
 class Pokemon
+  attr_accessor :name, :type, :id, :db
+
+  def initialize(name:, type:, db:, id:)
+    @name = name
+    @type = type
+    @db = db
+    @id = id
+  end
+
+  def self.find(id_num, db)
+    sql = 'SELECT * from pokemon WHERE id = ?'
+    info = db.execute(sql, id_num).flatten
+    new(id: info[0], name: info[1], type: info[2], db: db)
+  end
+
+  def self.save(name, type, db)
+    sql = 'INSERT INTO pokemon (name, type) VALUES (?, ?)'
+    db.execute(sql, name, type)
+  end
 end
--- spec/pokemon_spec.rb
+++ spec/pokemon_spec.rb
@@ -1,16 +1,16 @@
-require_relative "spec_helper"
+require_relative 'spec_helper'
 
-describe "Pokemon" do
+describe 'Pokemon' do
   before do
     @db = SQLite3::Database.new(':memory:')
-    @db.execute("DROP TABLE IF EXISTS pokemon")
+    @db.execute('DROP TABLE IF EXISTS pokemon')
     @sql_runner = SQLRunner.new(@db)
     @sql_runner.execute_schema_migration_sql
   end
 
-  let(:pokemon) {Pokemon.new(id: 1, name: "Pikachu", type: "electric", db: @db)}
+  let(:pokemon) { Pokemon.new(id: 1, name: 'Pikachu', type: 'electric', db: @db) }
 
-  describe ".initialize" do
+  describe '.initialize' do
     it 'is initialized with keyword arguments of id, name, type and db' do
       expect(pokemon).to respond_to(:id)
       expect(pokemon).to respond_to(:name)
@@ -19,25 +19,24 @@ describe "Pokemon" do
     end
   end
 
-  describe ".save" do
+  describe '.save' do
     it 'saves an instance of a pokemon with the correct id' do
-      Pokemon.save("Pikachu", "electric", @db)
+      Pokemon.save('Pikachu', 'electric', @db)
 
       pikachu_from_db = @db.execute("SELECT * FROM pokemon WHERE name = 'Pikachu'")
-      expect(pikachu_from_db).to eq([[1, "Pikachu", "electric"]])
+      expect(pikachu_from_db).to eq([[1, 'Pikachu', 'electric']])
     end
   end
 
-  describe ".find" do
+  describe '.find' do
     it 'finds a pokemon from the database by their id number and returns a new Pokemon object' do
       # The find method creates a new Pokemon after selecting their row from the database by their id number.
-      Pokemon.save("Pikachu", "electric", @db)
+      Pokemon.save('Pikachu', 'electric', @db)
 
       pikachu_from_db = Pokemon.find(1, @db)
       expect(pikachu_from_db.id).to eq(1)
-      expect(pikachu_from_db.name).to eq("Pikachu")
-      expect(pikachu_from_db.type).to eq("electric")
+      expect(pikachu_from_db.name).to eq('Pikachu')
+      expect(pikachu_from_db.type).to eq('electric')
     end
   end
-
 end
